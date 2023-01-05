3736419f6d38a4dda8d2719d0cd53380842c67f5
Ahmad <achmed.alsabbgh@gmail.com>
2023-01-05 22:29:03 +0100

    made a start with the index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const apiKey = "563492ad6f91700001000001ca06e45aef8d4018b4e7015052fa73fd";
+const searchImage = (query) => {
+  const apiKey = "563492ad6f91700001000001ca06e45aef8d4018b4e7015052fa73fd";
+  const url = `https://api.pexels.com/v1/search?query=${query}&per_page=80`;
+  fetch(url, {
+    method: "get",
+    headers: {
+      Accept: "application/json",
+      Authorization: apiKey,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
+searchImage("puppy");
