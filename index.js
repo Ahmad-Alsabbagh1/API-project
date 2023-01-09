12f3efe57a6679749de62cb909c537234f7811ec
Ahmad <achmed.alsabbgh@gmail.com>
2023-01-09 14:39:11 +0100

    Added media queries

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const searchImage = (query = "nature") => {
+const searchImage = (query) => {
   const apiKey = "563492ad6f91700001000001ca06e45aef8d4018b4e7015052fa73fd";
-  const url = `https://api.pexels.com/v1/search?query=${query}&per_page=80&orientation=landscape`;
+  const url = `https://api.pexels.com/v1/search?query=${query}&per_page=80&orientation=landscape&page=1`;
   fetch(url, {
     method: "get",
     headers: {
@@ -13,15 +13,15 @@ const searchImage = (query = "nature") => {
     .catch((error) =>
       alert(
         "Oops, something went wrong: " +
-          error +
-          ", Check internet connection and refresh the page"
+          "Make sure the search bar is not empty, otherwise check internet connection and refresh the page. " +
+          error
       )
     );
 };
 
 const showImages = (pictures) => {
   const wallpapers = pictures.photos;
-
+  console.log(pictures);
   wallpapers.forEach((pic) => {
     appendImage(pic.src.large, pic.alt, pic.photographer, pic.url);
   });
@@ -32,16 +32,17 @@ const appendImage = (url, alt, photographer, href) => {
   const picDiv = document.createElement("div");
   const pic = document.createElement("img");
   const link = document.createElement("a");
+  pic.src = url;
+  pic.alt = alt;
+  pic.title = photographer; //To see the photographer name when hovering the wallpaper
   link.href = href;
   link.target = "_blank"; //To open image in new tab
   link.appendChild(pic);
-  pic.setAttribute("id", "wallpaper");
-  pic.src = url;
-  pic.alt = alt;
-  pic.title = photographer;
+  pic.setAttribute("class", "wallpapers");
   picDiv.appendChild(link);
   picContainer.appendChild(picDiv);
 };
+
 window.onload = () => {
   document.querySelector(".loader").style.display = "none";
   const searchField = document.querySelector("#search");
